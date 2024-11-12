e71488534a23eafe0ab2499b9d0b75001bad7a5f
robertdepweg <rdepweg04@gmail.com>
2024-11-12 18:01:11 -0500

    Add GUI load CSV options.

--- gui_user_interface.py
+++ gui_user_interface.py
@@ -6,6 +6,7 @@ import FreeSimpleGUI as sg
 # First-Party Imports
 from errors import AlreadyImportedError
 
+
 class BeverageListWindow:
     """Simple Beverage List"""
 
@@ -17,6 +18,129 @@ class BeverageListWindow:
 
         layout = [
             [sg.Text("Beverage List")],
+            [
+                sg.Listbox(
+                    ["No Beverages"], 
+                    key="-output-", 
+                    size=(150, 34),
+                    font="TkFixedFont",
+                )
+            ],
+            [
+                sg.Button("Load CSV", key="-load_csv-"),
+                sg.Button("Add New Beverage", key="-open_add_new"),
+                sg.Input("", key="-search_id-"),
+                sg.Button("search", key="-search_button-"),
+            ],
+            [sg.Button("Exit")],
         ]
 
         self.window = sg.Window("Beverage List", layout)
+
+    def run(self):
+        """Start the window up"""
+        self._run_loop()
+        self.window.close()
+
+    def _run_loop(self):
+        """Run the event loop"""
+        while True:
+            event, values = self.window.read()
+            if event == sg.WINDOW_CLOSED or event == "Exit":
+                break
+            elif event == "-load_csv-":
+                self._on_load_csv(event, values)
+            elif event == "-open_add_new-":
+                self._on_open_add_new(event, values)
+            elif event == "-search_button-":
+                self._on_search(event, values)
+
+    def _on_load_csv(self, event, values):
+        """Handle when load CSV button is clicked"""
+        try:
+            self.csv_processor.import_csv(
+                self.beverage_collection, 
+                self.path_to_csv,
+            )
+            self._update_output(event, values)
+            sg.popup_ok("Beverage list has been imported successfully.")
+
+        except AlreadyImportedError:
+            sg.popup_error("The CSV file has already been imported.")
+        except FileNotFoundError:
+            sg.popup_error("File not found for opening.")
+        except EOFError:
+            sg.popup_error("The file was unchanged.")
+
+    def on_open_add_new(self, event, values):
+        """Open a new window to allow adding a new beverage"""
+        add_window = BeverageAddWindow(self.beverage_collection)
+        add_window.run()
+        self._update_output(event, values)
+
+    def _on_search(self, event, values):
+        """Handle when user is searching for an item"""
+
+    def _update_output(self, event, values):
+        """Update then contents of the output box"""
+        self.window["-output-"].update(self.beverage_collection.beverages)
+
+
+class BeverageAddWindow:
+    """Simple Beverage Add"""
+
+    def __init__(self, beverage_collection):
+        """Constructor"""
+        self.beverage_collection = beverage_collection
+
+        layout = [
+            [sg.Text("Id", size=(8,1)), sg.Input(key="-id-")],
+            [sg.Text("Name", size=(8,1)), sg.Input(key="-name-")],
+            [sg.Text("Pack", size=(8,1)), sg.Input(key="-pack-")],
+            [sg.Text("Price", size=(8,1)), sg.Input(key="-price-")],
+            [
+                sg.Radio(
+                    "Active",
+                    "beverage_active_group",
+                    key="-beverage_active-",
+                    default=True,
+                )
+            ],
+            [
+                sg.Radio(
+                    "Inactive", 
+                    "beverage_active_group", 
+                    key="-beverage_inactive-"
+                )
+            ],
+            [
+                sg.Button("Add", key="-add_new-"),
+                sg.Button("Cancel", key="-cancel-"),
+            ],
+        ]
+        self.window = sg.Window("Beverage Add", layout)
+
+    def run(self):
+        """Start the window"""
+        self._run_loop()
+        self.window.close()
+
+    def _run_loop(self):
+        """Run the Event loop"""
+        while True:
+            event, values = self.window.read()
+            if event == sg.WINDOW_CLOSED or event == "-cancel-":
+                break
+            elif event == "-add_new-":
+                self._on_add_new(event, values)
+                break
+
+    def _on_add_new(self, event, values):
+        """Handle when Add button is clicked"""
+        self.beverage_collection.add(
+            values["-id-"],
+            values["-name-"],
+            values["-pack-"],
+            float(values["-price-"]), # Not good practice
+            values["-beverage_active-"] is True,
+        )
--- program.py
+++ program.py
@@ -6,7 +6,7 @@
 
 # Internal imports.
 from beverage import BeverageCollection
-from errors import AlreadyImportedError
+from gui_user_interface import BeverageListWindow
 from user_interface import UserInterface
 from utils import CSVProcessor
 
@@ -26,59 +26,12 @@ def main(*args):
     # Create an instance of the CSVProcessor class.
     csv_processor = CSVProcessor()
 
-    # Display the Welcome Message to the user.
-    ui.display_welcome_greeting()
+    # Create and run main window
+    main_window = BeverageListWindow(
+        beverage_collection,
+        csv_processor,
+        PATH_TO_CSV,
+    )
+    main_window.run()
+    
 
-    # Display the Menu and get the response. Store the response in the choice
-    # integer. This is the 'primer' run of displaying and getting.
-    choice = ui.display_menu_and_get_response()
-
-    # While the choice is not exit program
-    while choice != 5:
-        if choice == 1:
-            # Load the CSV File
-            try:
-                csv_processor.import_csv(beverage_collection, PATH_TO_CSV)
-                ui.display_import_success()
-
-            except AlreadyImportedError:
-                ui.display_already_imported_error()
-            except FileNotFoundError:
-                ui.display_file_not_found_error()
-            except EOFError:
-                ui.display_empty_file_error()
-
-        elif choice == 2:
-            # Print Entire List Of Items
-            all_item_string = str(beverage_collection)
-            if all_item_string:
-                ui.display_all_items(all_item_string)
-            else:
-                ui.display_all_items_error()
-
-        elif choice == 3:
-            # Search for an Item
-            search_query = ui.get_search_query()
-            item_info = beverage_collection.find_by_id(search_query)
-            if item_info:
-                ui.display_item_found(item_info)
-            else:
-                ui.display_item_found_error()
-
-        elif choice == 4:
-            # Collect information for a new item and add it to the collection
-            new_item_info = ui.get_new_item_information()
-            if beverage_collection.find_by_id(new_item_info[0]) is None:
-                beverage_collection.add(
-                    new_item_info[0],
-                    new_item_info[1],
-                    new_item_info[2],
-                    float(new_item_info[3]),
-                    new_item_info[4] == "True",
-                )
-                ui.display_add_beverage_success()
-            else:
-                ui.display_beverage_already_exists_error()
-
-        # Get the new choice of what to do from the user.
-        choice = ui.display_menu_and_get_response()
